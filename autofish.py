d8095bb71d0cdfb55274f988308d68ce5e1493f6
John Goh <johngohrw@gmail.com>
2021-09-11 15:13:38 +0800

    STEADYAF. code cleanup

--- autofish.py
+++ autofish.py
@@ -5,9 +5,12 @@ import numpy as np
 import time
 method = cv2.TM_CCOEFF_NORMED
 
+showBoundingBoxes = False
+printXLocations = False
+printClickEvents = False
+
 # load template matching images
 bite = cv2.imread('templates/gotabite3.png', cv2.IMREAD_UNCHANGED)
-
 left1 = cv2.imread('templates/proper/left_1.png')
 left2 = cv2.imread('templates/proper/left_2.png')
 left3 = cv2.imread('templates/proper/left_3.png')
@@ -26,9 +29,6 @@ right_templates = [right1, right2, right3, right4]
 cursor_templates = [cursor1, cursor2, cursor3, cursor4]
 
 bite_rows,bite_cols = bite.shape[:2]
-# leftbound_rows,leftbound_cols = leftbound.shape[:2]
-# rightbound_rows,rightbound_cols = rightbound.shape[:2]
-# cursor_rows,cursor_cols = cursor.shape[:2]
 
 # template matching threshold
 threshold = 0.8
@@ -50,7 +50,6 @@ while True:
     if (genshinWindow and genshinWindow.top > -100 and genshinWindow.left > -100):
         # genshin window is active
         w, h, x, y =  genshinWindow.width, genshinWindow.height, genshinWindow.left, genshinWindow.top
-        # print(x, y, w, h)
 
         # get screengrab of genshin window region with 
         # window title bar cropped, assuming titlebar is 24px in height.
@@ -58,7 +57,7 @@ while True:
         genshinTopRegion = pyautogui.screenshot(region=(x + (w//2) - 250, y + 120, 500, 130))
         genshinTopRegion = cv2.cvtColor(np.array(genshinTopRegion), cv2.COLOR_RGB2BGR)
 
-        # auto clicking upon bite
+        # Search for "bite" message and auto clicking upon match
         b_cache_delay += 1
         b_val = 0
         if (b_cache_delay > bite_cache_delay_threshold):
@@ -66,12 +65,13 @@ while True:
             _, b_val, _, b_x = cv2.minMaxLoc(bite_result)
         b_cache_delay += 1
         if (b_val >= bite_threshold and b_val <= 1 and b_cache_delay > bite_cache_delay_threshold):
-            print("bite detected, clicking for 0.5 sec")
-            cv2.rectangle(genshinTopRegion, b_x,(b_x[0]+bite_cols,b_x[1]+bite_rows),(0,255,0),1)
+            print("bite detected, start fishing")
             pyautogui.mouseDown()
             time.sleep(0.5)
             pyautogui.mouseUp()
             b_cache_delay = 0
+            if (showBoundingBoxes):
+                cv2.rectangle(genshinTopRegion, b_x,(b_x[0]+bite_cols,b_x[1]+bite_rows),(0,255,0),1)
             
         # # match template with whatever's on screen
         leftmax = 0
@@ -83,8 +83,9 @@ while True:
                 left_rows,left_cols = image.shape[:2]
         l_val = leftmax
         if (l_val >= threshold):
-            cv2.rectangle(genshinTopRegion, l_x,(l_x[0]+left_cols,l_x[1]+left_rows),(0,0,255),1)
             l_x_cache = l_x
+            if (showBoundingBoxes):
+                cv2.rectangle(genshinTopRegion, l_x,(l_x[0]+left_cols,l_x[1]+left_rows),(0,0,255),1)
             
         rightmax = 0
         for image in right_templates:
@@ -95,8 +96,9 @@ while True:
                 right_rows,right_cols = image.shape[:2]
         r_val = rightmax
         if (r_val >= threshold):
-            cv2.rectangle(genshinTopRegion, r_x,(r_x[0]+right_cols,r_x[1]+right_rows),(0,0,255),1)
             r_x_cache = r_x
+            if (showBoundingBoxes):
+                cv2.rectangle(genshinTopRegion, r_x,(r_x[0]+right_cols,r_x[1]+right_rows),(0,0,255),1)
         
         cursormax = 0
         for image in cursor_templates:
@@ -107,8 +109,9 @@ while True:
                 cursor_rows,cursor_cols = image.shape[:2]
         c_val = cursormax
         if (c_val >= threshold):
-            cv2.rectangle(genshinTopRegion, c_x,(c_x[0]+cursor_cols,c_x[1]+cursor_rows),(0,0,255),1)
             c_x_cache = c_x
+            if (showBoundingBoxes):
+                cv2.rectangle(genshinTopRegion, c_x,(c_x[0]+cursor_cols,c_x[1]+cursor_rows),(0,0,255),1)
         
         # process caches if its not found
         if (l_val < threshold):
@@ -138,18 +141,21 @@ while True:
                 r_x = None
                 r_x_cache = None
 
-        print(l_x, " - ", c_x," - ", r_x)
+        if (printXLocations):
+            print(l_x, " - ", c_x," - ", r_x)
         
         if (r_x and c_x):
             rx = r_x[0]
             cx = c_x[0]
             if (cx + 100 < rx):
-                print("    <  I------->")
+                if (printClickEvents):
+                    print("    <  I------->")
                 pyautogui.mouseDown()
                 time.sleep(0.2)
                 pyautogui.mouseUp()
             elif (cx + 40 < rx):
-                print("    <     I---->")
+                if (printClickEvents):
+                    print("    <     I---->")
                 pyautogui.mouseDown()
                 time.sleep(0.02)
                 pyautogui.mouseUp()
@@ -158,12 +164,15 @@ while True:
             lx = l_x[0]
             cx = c_x[0]
             if (cx < lx):
-                print("I---<          >")
+                if (printClickEvents):
+                    print("I---<          >")
                 pyautogui.mouseDown()
-                time.sleep(0.2)
+                time.sleep(0.3)
                 pyautogui.mouseUp()
 
-        cv2.imshow('output', genshinTopRegion)
-        key = cv2.waitKey(1)
-        if key == 27:
-            cv2.destroyAllWindows()
+        
+        if (showBoundingBoxes):
+            cv2.imshow('output', genshinTopRegion)
+            key = cv2.waitKey(1)
+            if key == 27:
+                cv2.destroyAllWindows()
